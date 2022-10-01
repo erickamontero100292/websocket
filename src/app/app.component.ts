1a72a88d3840073ab23f9c719cd987bd60ae86b0
eamontero <monteroh.ericka@gmail.com>
2022-10-01 12:21:18 -0500

    add alert component add OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {WebsocketService} from "./websocket.service";
 import {MatDialog} from "@angular/material/dialog";
 import {AlertComponent} from "./alert/alert.component";
+import {Subscription} from "rxjs";
 
 export interface IChat {
 
@@ -17,40 +18,54 @@ export interface IChat {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
   title = 'websocket';
+  subs: Subscription[] = [];
 
-  messageList: IChat[]=[];
+  messageList: IChat[] = [];
 
   modelForm = this._fb.group({
     message: ['', [Validators.required]]
   });
+
   constructor(private _fb: FormBuilder, private websocketService: WebsocketService, private dialog: MatDialog) {
 
   }
 
-  send() {
-  console.log(this.modelForm.value.message);
-  this.dialog.open(AlertComponent);
 
-    var chat = {} as IChat;
-    chat.text = <string>this.modelForm.value.message;
-    chat.date = new Date;
-    chat.name ='eamontero';
-    this.messageList.push(chat);
-    this.websocketService.ws.send(JSON.stringify(chat));
-    this.modelForm.reset();
+  send() {
+    console.log(this.modelForm.value.message);
+    var sub = this.dialog.open(AlertComponent).afterClosed().subscribe(x => {
+
+      if (x) {
+        var chat = {} as IChat;
+        chat.text = <string>this.modelForm.value.message;
+        chat.date = new Date;
+        chat.name = 'eamontero';
+        this.messageList.push(chat);
+        this.websocketService.ws.send(JSON.stringify(chat));
+        this.modelForm.reset();
+      }
+    });
+
+    this.subs.push(sub);
   }
 
   ngOnInit(): void {
 
-    this.websocketService.message.subscribe((message )=>{
+    this.websocketService.message.subscribe((message) => {
       console.log('Desde el componente: ' + message);
       var chat = {} as IChat;
       chat.text = message.text;
       chat.date = new Date;
-      chat.name= message.name;
+      chat.name = message.name;
       this.messageList.push(chat);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => {
+      sub.unsubscribe();
+    })
   }
 }
